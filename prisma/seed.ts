d900abed26121bc950b4e1dca25a7831a69bed82
Muhammad Tsany Qudsi <tsanyqudsi@diamanti.dev.local>
2020-02-13 21:22:35 +0700

    add name for types

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,6 +5,7 @@ async function main() {
   const user1 = await prisma.user.create({
     data: {
       email: 'admin@gmail.com',
+      name: 'Administrator',
       password: '$2b$10$/1CrKPoRuMweAKDbR9s4xOdOy.cbcUOLkgk7KMBpkyExFQ.afnopq', // 1n14dmin
       role: 'ADMIN',
       class_year: 2000,
@@ -14,6 +15,7 @@ async function main() {
   const user2 = await prisma.user.create({
     data: {
       email: 'member@gmail.com',
+      name: 'Member Pertamax',
       password: '$2b$10$/1CrKPoRuMweAKDbR9s4xOdOy.cbcUOLkgk7KMBpkyExFQ.afnopq', // 1n14dmin
       role: 'MEMBER',
       class_year: 2000,
